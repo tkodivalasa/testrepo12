11868e483c724c84a95902465aac0fbac328c8d9
thriveni <veniyadav9854@gmail.com>
2020-11-17 18:27:37 +0530

    platform added

--- sketch.js
+++ sketch.js
@@ -34,6 +34,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    platform = new Ground(150,300,300,180)
 
 }
 
@@ -59,4 +60,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
